9aaec805e5d2cabf002c1d07c05c594a579c972a
bhatpranav <pranavbhat002@gmail.com>
2021-05-21 17:46:16 +0530

    Created quick_sort_recursive.cpp

---
+++ quick_sort_recursive.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int l, int r)
+{
+    int pivot = arr[r]; // pivot
+    int i = l - 1;      // Index of smaller element and indicates the right position of pivot found so far
+
+    for (int j = l; j < r; j++)
+    {
+        // If current element is smaller than the pivot
+        if (arr[j] < pivot)
+        {
+            i++; // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[r]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int pi = partition(arr, l, r);
+        quickSort(arr, l, pi - 1);
+        quickSort(arr, pi + 1, r);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array = ";
+    cin >> n;
+    if (n == 0)
+    {
+        cout << "The Array is Empty!!" << endl;
+        return 0;
+    }
+    int arr[n];
+    cout << "Enter the elements : " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter element " << i + 1 << " = ";
+        cin >> arr[i];
+    }
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted Array =  ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
