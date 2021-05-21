3360450e0e745893c989a37fc493d949a70e11d8
bhatpranav <pranavbhat002@gmail.com>
2021-05-21 14:54:20 +0530

    Created stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,54 +1,90 @@
-#include <iostream>
+/* C++ program to implement basic stack
+operations */
+#include <bits/stdc++.h>
 
-void push(int *arr, int top, int val)
+using namespace std;
+
+#define MAX 1000
+
+class Stack
 {
-    int stack_size = sizeof(arr) / sizeof(arr[0]);
-    if (top == stack_size - 1)
+    int top;
+
+public:
+    int a[MAX]; // Maximum size of Stack
+
+    Stack() { top = -1; }
+    bool push(int x);
+    int pop();
+    int peek();
+    bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
+    if (top >= (MAX - 1))
     {
-        std::cout << "Stack is Full!!" << std::endl;
+        cout << "Stack Overflow";
+        return false;
     }
     else
     {
-        ++top;
-        arr[top] = val;
+        a[++top] = x;
+        cout << x << " pushed into stack\n";
+        return true;
     }
 }
 
-int pop(int *arr, int top)
+int Stack::pop()
 {
     if (top < 0)
     {
-        std::cout << "Stack is Empty!!!" << std::endl;
+        cout << "Stack Underflow";
+        return 0;
     }
     else
     {
-        return arr[top];
+        int x = a[top--];
+        return x;
     }
 }
 
-bool isFull(int *arr, int top)
-{
-    int stack_size = sizeof(arr) / sizeof(arr[0]);
-    if (top == stack_size - 1)
-    {
-        return true;
-    }
-    return false;
-}
-
-bool isEmpty(int top)
+int Stack::peek()
 {
     if (top < 0)
     {
-        return true;
+        cout << "Stack is Empty";
+        return 0;
     }
-    return false;
+    else
+    {
+        int x = a[top];
+        return x;
+    }
 }
 
+bool Stack::isEmpty()
+{
+    return (top < 0);
+}
+
+// Driver program to test above functions
 int main()
 {
-    int n{0};
-    std::cout << "Enter the size of the stack: ";
-    std::cin >> n;
-    int stack[n];
+    class Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    cout << s.pop() << " Popped from stack\n";
+    //print all elements in stack :
+    cout << "Elements present in stack : ";
+    while (!s.isEmpty())
+    {
+        // print top element in stack
+        cout << s.peek() << " ";
+        // remove top element in stack
+        s.pop();
+    }
+
+    return 0;
 }
